1f825f5e8411b343d39b8ce6e7b6d34201c16f5c
Robert Wall <robert@rww.name>
2013-09-28 14:55:19 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Pwtest::Application.config.secret_key_base = 'cf454f96ea9c4c0def4380bc4c07c3357928ed6c7cdbe5b93e241bbf0417d8b9b3e3f9c595bd1b706a52383d148a582f2210f60c6d467bc7895a3dc68953979e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Pwtest::Application.config.session_store :cookie_store, key: '_pwtest_session'
