815896b5d79d5790676f05148ae39ef2ef8f9521
victorfackrell <hardicevl@gmail.com>
2013-10-12 13:20:21 -0700

    Make Gender Nullable v2

--- db/migrate/20131012201928_change_gender_column2.rb
+++ db/migrate/20131012201928_change_gender_column2.rb
@@ -1,4 +1,4 @@
-class ChangeGenderColumn < ActiveRecord::Migration
+class ChangeGenderColumn2 < ActiveRecord::Migration
   def change
     change_column :students, :gender, :string, :null => true
   end
