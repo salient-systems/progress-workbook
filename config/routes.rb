8b505372d8cb3988bfbd590494aee33ec8179a21
David <dtk538@gmail.com>
2013-10-12 16:08:42 -0700

    added angular and sample todo app

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Pwtest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static_pages#home'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
