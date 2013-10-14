807aed0224a23d481e33295b3bafaa39971fe695
David <dtk538@gmail.com>
2013-10-13 20:58:46 -0700

    individual student detail pages fixed navigation links so back button works fixed student id not being included in json reponse (this will need to be done for all resources eventually)

--- app/assets/javascripts/progress-workbook.js
+++ app/assets/javascripts/progress-workbook.js
@@ -1,24 +1,44 @@
-/* 
+/*
  * Maps routes to controllers. Notice that the controller
  * module gets passed as an argument into the app constructor.
- */ 
+ */
 var app = angular.module('pw', ['controllers']);
+
 app.config(function($routeProvider) {
   $routeProvider.
-      when('/classes', {templateUrl: 'templates/classes.html'}).
-      when('/class', {templateUrl: 'templates/class.html', controller: 'ClassCtrl'}).
-      when('/students', {templateUrl: 'templates/students.html'}).
-      when('/performance', {templateUrl: 'templates/performance.html'}).
-      when('/users', {templateUrl: 'templates/users.html', controller: 'UserCtrl'}).
-      when('/cohorts', {templateUrl: 'templates/cohorts.html'}).
-      otherwise({redirectTo: '/classes'});
+    when('/classes', {
+      templateUrl: 'templates/classes.html'
+    }).
+    when('/class', {
+      templateUrl: 'templates/class.html',
+      controller: 'ClassCtrl'
+    }).
+    when('/students', {
+      templateUrl: 'templates/students.html',
+      controller: 'StudentListCtrl'
+    }).
+    when('/students/:id', {
+      templateUrl: 'templates/student.html',
+      controller: 'StudentCtrl'
+    }).
+    when('/performance', {
+      templateUrl: 'templates/performance.html'
+    }).
+    when('/users', {
+      templateUrl: 'templates/users.html',
+      controller: 'UserListCtrl'
+    }).
+    when('/cohorts', {
+      templateUrl: 'templates/cohorts.html'
+    }).
+    otherwise({redirectTo: '/classes'});
 });
 
 /*
- * Create REST resources. The argument to 
+ * Create REST resources. The argument to
  * the $resource function is the url of the REST API
  * corresponding to that resource.
- */ 
+ */
 var rest = angular.module('rest', ['ngResource']);
 
 rest.factory('Users', function($resource) {
@@ -26,7 +46,7 @@ rest.factory('Users', function($resource) {
 });
 
 rest.factory('Students', function($resource) {
-  return $resource('/students', {});
+  return $resource('/students/:id', {});
 });
 
 /*
@@ -36,19 +56,44 @@ rest.factory('Students', function($resource) {
  */
 var controllers = angular.module('controllers', ['rest']);
 
-controllers.controller('UserCtrl', ['$scope', 'Users', function($scope, Users) {
-	$scope.data	= {};
-	
-	Users.query(function(response) {
-		$scope.data.users = response;
-	});
-}]);
+// user list
+controllers.controller('UserListCtrl', ['$scope', 'Users',
+  function($scope, Users) {
+  	$scope.data	= {};
 
-controllers.controller('ClassCtrl', ['$scope', 'Students', function($scope, Students) {
-  $scope.data = {};
-  
-  Students.query(function(response) {
-    $scope.data.students = response;
-  });
-}]);
+  	Users.query(function(response) {
+  		$scope.data.users = response;
+  	});
+  }]);
 
+// class details
+controllers.controller('ClassCtrl', ['$scope', 'Students',
+  function($scope, Students) {
+    $scope.data = {};
+
+    Students.query(function(response) {
+      $scope.data.students = response;
+    });
+  }]);
+
+// student list
+controllers.controller('StudentListCtrl', ['$scope', '$location', 'Students',
+  function($scope, $location, Students) {
+    $scope.data = {};
+
+    Students.query(function(response) {
+      $scope.data.students = response;
+    });
+
+    /*$scope.view = function(student) {
+      console.log('#/students/' + student.id);
+      $location.path('/students/' + student.id);
+    };*/
+  }]);
+
+// student details
+controllers.controller('StudentCtrl', ['$scope', '$routeParams', 'Students',
+  function($scope, $routeParams, Students) {
+    $scope.student = Students.get({id: $routeParams.id});
+    console.log($scope.student);
+  }]);
