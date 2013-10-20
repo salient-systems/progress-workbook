58dcbdb5ab188604b396f88a37ae5a51c7927f4b
Robert Wall <robert@rww.name>
2013-10-20 16:55:43 -0700

    Add seed.rb code for Users, Subject, Sections, and Students

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Numbers chosen by taking SRS estimates and doubling them
+NUM_USERS = 100
+NUM_SUBJECTS = 20
+NUM_SECTIONS = 500
+NUM_STUDENTS = 2000
+
+1.upto(NUM_USERS) do |num|
+	User.create(fname: "TestUser",
+		    lname: "Number#{num}",
+		    username: "user#{num}",
+		    password: "pass#{num}",
+		    is_active: num % 11 != 0,
+		    is_admin: num % 7 == 0)
+end
+
+1.upto(NUM_SUBJECTS) do |num|
+	Subject.create(name: "Subject #{num}")
+end
+
+1.upto(NUM_SECTIONS) do |num|
+	Section.create(name: "Underwater Basketweaving Section #{num}",
+		       grade_level: num % 3 + 6,
+		       start_date: "07/01/2013",
+		       end_date: "06/01/2014",
+		       user_id: num % NUM_USERS + 1,
+		       subject_id: num % NUM_SUBJECTS + 1)
+end
+
+1.upto(NUM_STUDENTS) do |num|
+	student = Student.create(fname: "TestStudent",
+		       lname: "Number#{num}",
+		       gender: (num % 2 == 0 ? "m" : "f"),
+		       grade_level: num % 3 + 6,
+		       is_active: num % 11 != 0)
+end
