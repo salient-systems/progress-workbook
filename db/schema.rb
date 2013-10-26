5a45ae1e0feee43d40d8f7a8e40d03dad251e93a
David <dtk538@gmail.com>
2013-10-26 16:47:35 -0700

    blah

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20131019184620) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "assessment_grades", force: true do |t|
     t.float    "total"
     t.integer  "assessment_id", null: false
