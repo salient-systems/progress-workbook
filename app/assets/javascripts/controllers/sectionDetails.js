caf1c7b07015d4d5bab1757350c06872587cffc8
MasterDevwi <william@devereuxonline.com>
2013-10-28 21:51:44 -0700

    Added class name and subtitle (template) to section page.

--- app/assets/javascripts/controllers/sectionDetails.js
+++ app/assets/javascripts/controllers/sectionDetails.js
@@ -2,6 +2,8 @@
 app.controller('ClassCtrl',
 function($scope, $routeParams, Restangular) {
   var section = Restangular.one('sections', $routeParams.id);
+  $scope.section = section.get();
   $scope.students = section.getList('students');
+  $scope.subjects = section.getList('subjects');
   $scope.assessment_types = section.getList('assessment_types');
 });
