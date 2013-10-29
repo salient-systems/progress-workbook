64e0385e344c332984d16e485d5d73a9729aa952
David <dtk538@gmail.com>
2013-10-28 22:31:33 -0700

    made cohort modal functional and fixed bug in student modal

--- app/assets/javascripts/controllers/cohortList.js
+++ app/assets/javascripts/controllers/cohortList.js
@@ -1,4 +1,15 @@
 // cohort list
 app.controller('CohortListCtrl', function($scope, Restangular) {
-  $scope.cohorts = Restangular.all('cohorts').getList();
+  Restangular.all('cohorts').getList().then(function(cohorts) {
+    $scope.cohorts = cohorts;
+  });
+
+  $scope.save = function() {
+    var newCohort = angular.copy($scope.newCohort);
+    Restangular.all('cohorts').post(newCohort).then(function(cohort) {
+      // response from server gives us id of new cohort
+      $scope.cohorts.push(cohort);
+    });
+    $scope.newCohort = null;
+  };
 });
