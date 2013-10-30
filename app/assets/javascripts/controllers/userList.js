a80f161aef0dfe1ca7cb61b4d7cfac51b7c4ba76
unknown <Daniel@.(none)>
2013-10-29 18:32:16 -0700

    Added parent controller, modal controller, and appropriate ng-model attributes for the "create user" modal dialog in users.html

--- app/assets/javascripts/controllers/userList.js
+++ app/assets/javascripts/controllers/userList.js
@@ -2,6 +2,9 @@
 app.controller('UserListCtrl', function($scope, Restangular) {
   $scope.users = Restangular.all('users').getList();
 
+	$scope.save = function() {		
+	};
+	
 	$scope.activateUsersButton = function() {
 		$scope.active = !$scope.active;
 	};
