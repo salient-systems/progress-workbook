0b4f11405c6e91ad7f2e61d7c77dbd9de1005c58
unknown <Daniel@.(none)>
2013-10-29 19:57:53 -0700

    Can now create/push new users to database

--- app/assets/javascripts/controllers/userList.js
+++ app/assets/javascripts/controllers/userList.js
@@ -1,11 +1,24 @@
 // user list
 app.controller('UserListCtrl', function($scope, Restangular) {
-  $scope.users = Restangular.all('users').getList();
-
-	$scope.save = function() {		
-	};
+  Restangular.all('users').getList().then(function(users) {
+    $scope.users = users;
+  });
 	
+	//used for when a user toggles the "show inactive/active" button
 	$scope.activateUsersButton = function() {
 		$scope.active = !$scope.active;
 	};
+});
+
+//controller for "create user" modal dialog 
+app.controller('AddUser', function($scope, Restangular) {
+  $scope.save = function() {
+    var newUser = angular.copy($scope.newUser); //copy model of user in fields
+    newUser.is_active = true; // TODO make is_active default to true in the DB
+    newUser.password = "we56h"; //TODO should implement password system, possibly here.
+    Restangular.all('users').post(newUser).then(function(response) {
+      $scope.users.push(response);
+    });
+    $scope.newUser = null; // reset the form
+  };
 });
