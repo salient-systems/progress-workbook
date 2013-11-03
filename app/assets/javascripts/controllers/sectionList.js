a79c3878c07876deff44fd0daca36934537e4dd6
victorfackrell <hardicevl@gmail.com>
2013-11-02 19:51:14 -0700

    Added Ng-grid to sectionList page

--- app/assets/javascripts/controllers/sectionList.js
+++ app/assets/javascripts/controllers/sectionList.js
@@ -2,43 +2,39 @@
 app.controller('ClassListCtrl', function($scope, Restangular) {
   $scope.sections = Restangular.all('sections').getList();
   
-  	/*$scope.mySelections = [];
-  	var editTemplate = '<input type="number" ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="save()" />';
-  	var nameTemplate = '<div class="ngCellText" ng-class="col.colIndex()"><a href="#/students/{{row.getProperty(\'id\')}}">{{COL_FIELD}}</a></div>';
+  $scope.mySelections = [];
+  var nameTemplate = '<div class="ngCellText" ng-class="col.colIndex()"><a href="#/classes/{{row.getProperty(\'id\')}}">{{COL_FIELD}}</a></div>';
+  var editTemplate = '<input type="number" ng-class="\'colt\' + col.index" ng-input="COL_FIELD" ng-model="COL_FIELD" ng-blur="save()" />';
   
-	$scope.gridOptions = {
+  $scope.gridOptions = {
     data: 'sections',
     selectedItems: $scope.mySelections,
     multiSelect: true,
     showSelectionCheckbox: true,
     selectWithCheckboxOnly: true,
-    //enableCellEditOnFocus: true, //assuming you dont want to edit displayed user information
+    enableCellSelection: false,
+    enableCellEditOnFocus: false,
     filterOptions: { filterText: '', useExternalFilter: false },
     columnDefs: [
-      //if you want to add a size of cohort column, you'll need to implement it here using the same 4 fields for Cohort column as seen below.
       {
         field: 'name',
         displayName:'Title',
         cellTemplate: nameTemplate,
         enableCellEdit: false,
       }, {
-        field: 'subject_id',
+        field: 'subject.name',
         displayName:'Subject',
-        cellTemplate: nameTemplate,
         enableCellEdit: false,
       }, {
         field: 'grade_level',
-        displayName: 'Grade',
-        cellTemplate: nameTemplate,
-        enableCellEdit: false,
+        displayName: 'Grade'
       },{
-        field: , 
+        field: 'user.fname',
         displayName: 'Teacher',
-        cellTemplate: nameTemplate,
-        enableCellEdit: false,
-      },
-        //displayName: 'Action', cellTemplate: '<a href="" ng-click="editUser(row.getProperty(\'id\'))"><i class="glyphicon glyphicon-pencil" />Edit</a>'
-      }
+        cellTemplate: '<div class="ngCellText" ng-class="col.colIndex()"><a href="#/users/{{row.getProperty(\'id\')}}">{{row.getProperty(\'user.fname\')}} {{row.getProperty(\'user.lname\')}}</a></div>'
+      },/*{
+        displayName: 'Action', cellTemplate: '<a href="" ng-click="editUser(row.getProperty(\'id\'))"><i class="glyphicon glyphicon-pencil" />Edit</a>'
+      }*/
     ],
     afterSelectionChange: function () {
       $scope.selectedIDs = [];
@@ -46,6 +42,6 @@ app.controller('ClassListCtrl', function($scope, Restangular) {
           $scope.selectedIDs.push(item.id);
       });
     }
-  };*/
+  };
   
 });
