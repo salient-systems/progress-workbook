b4ea9ec7839d761d3f42d8da0ad6348685145970
David <dtk538@gmail.com>
2013-11-02 17:14:52 -0700

    rudimentary student editing

--- app/assets/javascripts/controllers/studentDetails.js
+++ app/assets/javascripts/controllers/studentDetails.js
@@ -1,6 +1,13 @@
 // student details page
 app.controller('StudentCtrl', function($scope, $routeParams, Restangular) {
   var student = Restangular.one('students', $routeParams.id);
-  $scope.student = student.get();
+  student.get().then(function(thestudent) {
+    $scope.student = thestudent;
+    $scope.editStudent = angular.copy(thestudent);
+  });
   $scope.sections = student.getList('sections');
+
+  $scope.save = function() {
+    $scope.student.put();
+  };
 });
