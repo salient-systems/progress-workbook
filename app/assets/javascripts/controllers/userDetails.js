078c22b16f5dc865e7262131405a124526dbc2f3
victorfackrell <hardicevl@gmail.com>
2013-11-02 17:43:16 -0700

    Updated user update

--- app/assets/javascripts/controllers/userDetails.js
+++ app/assets/javascripts/controllers/userDetails.js
@@ -1,19 +1,11 @@
 // user details page
 app.controller('UserCtrl', function($scope, $routeParams, Restangular) {
   var user = Restangular.one('users', $routeParams.id);
-  user.get().then(function(theuser) {
-    $scope.user = theuser;
-    $scope.editUser = {
-      fname: theuser.fname,
-      lname: theuser.lname,
-      username: theuser.username,
-      is_admin: theuser.is_admin
-    };
+  user.get().then(function(thestudent) {
+    $scope.user = thestudent;
   });
-  $scope.sections = user.getList('sections');
 
   $scope.save = function() {
-    console.log($scope.editUser);
-    $scope.user.patch($scope.editUser);
+    $scope.user.put();
   };
 });
