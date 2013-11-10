76bb4f613b8dfed79044d11a4ce09b9ec54d037d
MasterDevwi <william@devereuxonline.com>
2013-11-09 17:37:29 -0800

    Added form validation to settings page and modals for adding a subject and advancing the term.

--- app/assets/javascripts/controllers/settings.js
+++ app/assets/javascripts/controllers/settings.js
@@ -1,60 +1,32 @@
-// student list
-app.controller('SettingsCtrl', function($scope, $rootScope, Restangular) {
-  if ($rootScope.students === undefined) {
-    Restangular.all('students').getList().then(function(students) {
-      $rootScope.students = students;
-      $scope.students = $rootScope.students;
-    });
-  } else {
-    $scope.students = $rootScope.students;
-  }
-
-  $scope.checked_students = [];
-  $scope.mySelections = [];
-
-  $scope.save = function() {
-    //console.log(row.entity);
-    //console.log(column.field);
-    //console.log('Cell Value prior: ' + row.entity[column.field]);
-    //console.log('Cell Value after: ' + cellValue);
-    this.row.entity.put();
+app.controller('SettingsCtrl', function($scope, Restangular) {
+  $scope.savePassword = function() {
+    //Restangular.all('subjects').post({name: $scope.newSubject});
+    $scope.newPassword = null; // reset the form
   };
 
-
-app.controller('AddTerm', function($scope, Restangular) {
   $scope.saveTerm = function() {
-    var newStudent = angular.copy($scope.newStudent);
-    newStudent.is_active = true; // TODO make is_active default to true in the DB
-    Restangular.all('students').post(newStudent).then(function(response) {
-      $scope.students.push(response);
-    });
-    $scope.newStudent = null; // reset the form
-    $('#addStudentModal').modal('hide');
+    Restangular.all('terms').post({name: $scope.newTerm});
+    $scope.newTerm = null; // reset the form
+    $('#advanceTermModal').modal('hide');
     $scope.resetTermValidation();
   };
 
   $scope.resetTermValidation = function() {
-    $scope.newName = null;
+    $scope.newTerm = null;
+    $scope.confirmCheckbox = false;
+    $scope.validateTermName = false;
+    $scope.validateCheckbox = false;
   };
-});
 
-app.controller('AddSubject', function($scope, Restangular) {
   $scope.saveSubject = function() {
-    var newStudent = angular.copy($scope.newStudent);
-    newStudent.is_active = true; // TODO make is_active default to true in the DB
-    Restangular.all('students').post(newStudent).then(function(response) {
-      $scope.students.push(response);
-    });
-    $scope.newStudent = null; // reset the form
-    $('#addStudentModal').modal('hide');
+    Restangular.all('subjects').post({name: $scope.newSubject});
+    $scope.newSubject = null; // reset the form
+    $('#addSubjectModal').modal('hide');
     $scope.resetSubjectValidation();
   };
 
   $scope.resetSubjectValidation = function() {
-    $scope.newStudent = null;
-    $scope.validateFName = false;
-    $scope.validateLName = false;
-    $scope.validateSid = false;
-    $scope.validateGrade = false;
+    $scope.newSubject = null;
+    $scope.validateSubjectName = false;
   };
 });
