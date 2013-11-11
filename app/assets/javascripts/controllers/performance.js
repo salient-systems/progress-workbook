5f989e2edf6043f906f7ae6d051dc682d77638be
Daniel <sdrrust@gmail.com>
2013-11-10 20:59:08 -0800

    Updated performance page.

--- app/assets/javascripts/controllers/performance.js
+++ app/assets/javascripts/controllers/performance.js
@@ -1,6 +1,21 @@
 // performance details page
 app.controller('PerformanceCtrl', function($scope, $routeParams, Restangular) {
-
+  Restangular.all('sections').getList().then(function(thesection) {
+    $scope.sections = thesection;
+  });
+  Restangular.all('terms').getList().then(function(theterm) {
+    $scope.terms = theterm;
+  });
+  Restangular.all('assessment_types').getList().then(function(theassessmenttype) {
+    $scope.assessment_types = theassessmenttype;
+  });
+  Restangular.all('assessments').getList().then(function(theassessment) {
+    $scope.assessments = theassessment;
+  });
+  Restangular.all('criterions').getList().then(function(thecriterion) {
+    $scope.criterions = thecriterion;
+  });
+  //$('#inClass').css('visibility', 'hidden');
 });
 
 //chart controller
@@ -24,30 +39,39 @@ app.controller('ChartController', function($scope){
 });
 
 app.controller('accordionCtrl', function($scope){
+	
   $scope.panels = [
   	{
       dataSetName: 'Data Set 1',
       id: 1,
       open: true,
       searchCriteria: "",
-      schoolYear: "",
+      term: "",
       className: "",
+      statistic: "",
+      assessmentTypeName: "",
       assessmentName: "",
-      dataType: ""
-  	}];
+      criterion:  "" 	
+    }];
   	
   //this function will add a new data field when the save button is clicked
-  $scope.save = function() {
+  $scope.save = function(index) {
   	console.log($scope.panels.valueOf().length);
-    $scope.panels.push({
-    	dataSetName: "noob",
-    	id: $scope.panels.valueOf().length + 1,
-    	open: true,
-    	searchCriteria: "",
-      	schoolYear: "",
-      	className: "",
-      	assessmentName: "",
-      	dataType: ""
-    	});    
+  	console.log(index);
+  	//check if last panel clicking save is to open a 
+  	if(index == $scope.panels.valueOf().length - 1){
+	  	$scope.panels.push({
+	    	dataSetName: "noob",
+	    	id: $scope.panels.valueOf().length + 1,
+	    	open: true,
+	    	searchCriteria: "",
+	        term: "",
+	        className: "",
+	        statistic: "",
+	        assessmentTypeName: "",
+	        assessmentName: "",
+	        criterion: ""
+	    	});	
+  	}
   };
 });
