b351903e0cc1ffd992e988703c9034c6a1d3b04d
MasterDevwi <william@devereuxonline.com>
2013-11-16 13:15:39 -0800

    Moved increment grade option to a checkbox inside advance grade.

--- app/assets/javascripts/controllers/settings.js
+++ app/assets/javascripts/controllers/settings.js
@@ -4,13 +4,16 @@ app.controller('SettingsCtrl', function($scope, Restangular) {
     $scope.newPassword = null; // reset the form
   };
 
-  $scope.advanceSchoolYear = function() {
+  $scope.incrementGrades = function() {
   	Restangular.all('students').getList({advanceSchoolYear: 1337});
   };
 
   $scope.saveTerm = function() {
     Restangular.all('terms').post({name: $scope.newTerm});
     $scope.newTerm = null; // reset the form
+    if($scope.incGrades) {
+      $scope.incrementGrades();
+    }
     $('#advanceTermModal').modal('hide');
     $scope.resetTermValidation();
   };
@@ -18,6 +21,7 @@ app.controller('SettingsCtrl', function($scope, Restangular) {
   $scope.resetTermValidation = function() {
     $scope.newTerm = null;
     $scope.confirmCheckbox = false;
+    $scope.incGrades = false;
     $scope.validateTermName = false;
     $scope.validateCheckbox = false;
   };
